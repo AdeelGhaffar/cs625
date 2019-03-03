3edb67bc839fbd4dd54d14fc2f37e9b9c4246969
AdeelGhaffar <adeeladdi548@gmail.com>
2019-03-03 05:25:28 -0800

    error file

---
+++ WaterSong.java
@@ -0,0 +1,29 @@
+public class WaterSong {
+	public static void main(String[] args){
+		int waterNum = 9;
+		String word = "bottles";
+		while (waterNum > 0)
+		{
+			if (waterNum == 1)
+			{
+				word = "bottle";
+				
+			}
+			System.out.println(waterNum + " " + word + "of water on the wall");
+			System.out.println(waterNum + " " + word + "of water");
+			System.out.println("Take one down.");
+			System.out.println("pass it around.");
+			waterNum = waterNum - 1;
+			if (waterNum > 0)
+			{
+				System.out.println(waterNum +" " + word + " of water on the wall");
+			}
+                       else
+                        {
+                          System.out.println("No more bottles of water on the wall");
+                         
+                        }
+			
+		}
+	}
+}
