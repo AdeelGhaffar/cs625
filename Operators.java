55d06e551a5d434541599fb2393b204b4a423128
AdeelGhaffar <47943982+AdeelGhaffar@users.noreply.github.com>
2019-03-15 03:33:34 -0700

    Operators

---
+++ Operators.java
@@ -0,0 +1,16 @@
+public class Operators{
+public static void main(string[] args)
+{
+int a=20,b=10,c=0,d=20,e=40,f=30;
+boolean condition=true;
+c=++a;
+System.out.println("value of c(++a)="+c);
+c=b++;
+System.out.println("value of c(b++)="+C);
+c=--d;
+System.out.println("value of c(--d)="+c);
+c=--e;
+System.out.println("value of c(--e)"=+c);
+System.out.println("value of !condition="+!condition);
+}
+}
