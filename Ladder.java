51ac75893fb8d57e56981f26eddb5fe3c4b9cd1a
AdeelGhaffar <47943982+AdeelGhaffar@users.noreply.github.com>
2019-03-18 08:04:03 -0700

    Ladder

---
+++ Ladder.java
@@ -0,0 +1,17 @@
+public class Ladder{
+public static void main(String args[])
+{
+int x;
+for(x=0;x<6;x++){
+if(x==1)
+System.out.println("X is one:");
+else if(x==2)
+System.out.println("X is two");
+else if(x==3)
+System.out.println("X is three");
+else if(x==4)
+System.out.println("X is four");
+else 
+System.out.println("X is not between 1 & 4");
+}
+}
