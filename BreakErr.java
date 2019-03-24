b16f6eca008d393e816609a5903e69194a2718c0
AdeelGhaffar <47943982+AdeelGhaffar@users.noreply.github.com>
2019-03-23 23:24:00 -0700

    BreakErr

---
+++ BreakErr.java
@@ -0,0 +1,14 @@
+public class BreakErr{
+ public static void main(String[] args)
+{
+for(int i=0;i<3;i++)
+{
+System.out.println("Pass"+i+":");
+}
+for(int j;j<100;j++)
+{
+if(j==10)
+System.out.print(j+"");
+}
+}
+}
