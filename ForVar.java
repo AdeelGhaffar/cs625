e4128a3af3d4df47cd2f60f6732567ee8476a59c
AdeelGhaffar <47943982+AdeelGhaffar@users.noreply.github.com>
2019-03-23 23:34:24 -0700

    ForVar

---
+++ ForVar.java
@@ -0,0 +1,13 @@
+public class ForVar{
+ public static void main(String[] args)
+{
+int sum=0;
+int fact=1;
+for(int i=1;i<=5;i++)
+sum++i;
+fact*=i;
+}
+System.out.println("sum is"+sum);
+System.out.println("Factorial is"+fact);
+}
+}
