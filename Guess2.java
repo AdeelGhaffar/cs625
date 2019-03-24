be3c90ad00b069d37eaea14a79174dc4ec44179d
AdeelGhaffar <47943982+AdeelGhaffar@users.noreply.github.com>
2019-03-23 23:42:24 -0700

    Guess2

---
+++ Guess2.java
@@ -0,0 +1,17 @@
+public class Guess2{
+ public static void main(String[] args)
+throws java.io.IOException
+{
+char ch,answer='K';
+System.out.println("I'm thinking of a letter between A and Z:");
+System.out.println("Can you guess it:");
+ch=(char) System.in.read(); //get a char
+if(ch==answer) System.out.println("**Right**");
+else
+{
+System.out.println("...Sorry,You're");
+// a nested if
+if(ch<answer) System.out.println("Too high");
+}
+}
+}
